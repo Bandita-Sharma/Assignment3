0de4e44583805e8bbc5413fe2d20771f65086d0a
bandita <sharmabandita.105@gmail.com>
2018-08-17 14:54:32 +0530

    this is commit for string nd tuple

---
+++ strings.py
@@ -0,0 +1,15 @@
+#Que9-->Convert a String to Uppercase
+a=input("enter a string")
+print(a.upper())
+print()
+
+#Que10-->Check if string Contains all numeric Characters
+b=input("enter a string")
+print(b.isnumeric())
+print()
+
+#Que11-->Replace the word "World" with your name in the string "Hello World"
+c="Hello World"
+d=c.replace("World","Bandita")
+print(d)
+
---
+++ tuple.py
@@ -0,0 +1,7 @@
+#Que7-->Print a tuple in reverse order
+a=('apple','banana','mango','orange','cherry')
+print(a[ : :-1])
+
+#Que8-->Find largest and smallest element in a tuple
+print("Largest element in tuple:",max(a))
+print("Smallest element in tuple:",min(a))
